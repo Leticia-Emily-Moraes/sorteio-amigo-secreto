7c57260a0a4f156546e61c55a27420f70b0ff2a7
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2024-01-09 12:00:00 -0300

    Implemented a function to add friends

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-let listaDeAmigos = []
+let listaDeAmigos = [];
+
+function adicionarAmigo() {
+	let amigo = document.querySelector("input");
+	if (amigo.value == "") {
+		alert("Por favor, insira um nome.");
+	} else {
+		listaDeAmigos.push(amigo.value);
+		console.log("Amigo adicionado");
+		console.log(listaDeAmigos);
+	}
+	limparCampo();
+}
+
+function limparCampo() {
+	let amigo = document.querySelector("input");
+	amigo.value = "";
+}
